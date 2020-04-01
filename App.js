bcfd8e85f39b12e0e43db4b7c6bad6a5e5ad7d84
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2020-04-01 10:55:27 +0700

    move firebaseConfig to firebase folder

--- App.js
+++ App.js
@@ -5,18 +5,9 @@ import Navigator from './navigations/Navigator';
 import {AppLoading} from 'expo';
 import * as Font from 'expo-font';
 import * as firebase from 'firebase';
+import firebaseConfig from './firebase/firebaseConfig';
 
 const AppContainer = createAppContainer(Navigator);
-const firebaseConfig = {
-    apiKey: "AIzaSyCgHJXJe1oUoIucKlGmoFsVdHGCBOeoAaU",
-    authDomain: "kcashless-demo.firebaseapp.com",
-    databaseURL: "https://kcashless-demo.firebaseio.com",
-    projectId: "kcashless-demo",
-    storageBucket: "kcashless-demo.appspot.com",
-    messagingSenderId: "33620630259",
-    appId: "1:33620630259:web:659556fa935013d494b586",
-    measurementId: "G-6B246S53K3"
-};
 
 const fetchFonts = async () => {
     return Font.loadAsync({
